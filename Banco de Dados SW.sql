15ad2972b0cc571e415281795816e31d1648330d
KristhianWirthl <kristhian.wirthl@sptech.school>
2026-04-16 13:58:22 -0300

    adicionado o banco de dados e trabalhado nos sites

---
+++ Banco de Dados SW.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE STARWARS;
+
+USE STARWARS;
+
+CREATE TABLE usuario (
+    nome VARCHAR(45),
+    idade INT,
+    planeta_origem VARCHAR(100),
+    especie VARCHAR(100),
+    nave VARCHAR(50),
+    email VARCHAR(50),
+    senha VARCHAR(50),
+);
+
